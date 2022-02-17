fc484cfbfdbd7eb3a4d49ec7db653ae0a78b1b4b
Miguel <20207957@hope.ac.uk>
2022-02-17 19:12:11 +0000

    Miguel-Save1

---
+++ OOSD_Group_Game/Source.cpp
@@ -0,0 +1,22 @@
+#include "SFML/Graphics.hpp"
+
+
+
+int main()
+{
+	sf::RenderWindow window(sf::VideoMode(800, 800), "Game");
+	sf::Event e;
+
+	while (window.isOpen())
+	{
+		while (window.pollEvent(e))
+		{
+			if (e.type == sf::Event::Closed)
+				window.close();
+
+		}
+	}
+
+
+	return 0;
+}
